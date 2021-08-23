393747dc30b21e5ca3ac5ca538838e9e6888995a
Adam Kent <adamkentdev@gmail.com>
2021-08-23 10:18:20 -0800

    version 1.0

---
+++ intro.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+const firstName = "Adam";
+let age = 28;
+age = 100;
+console.log(firstName, age);
+
+let favRobot = 'Voltron';
+let favColor = `blue`;
+// concatenate strings with +
+console.log("Hello, my name is " + firstName);
+// concat with template literal
+console.log(`I am ${age} years old`);
+
+const isCool = true;
+
+let powerLevel;
+let weaknesses = null;
+console.log(powerLevel);
+
+if (age > 99){
+    console.log("Take away his driver's license!")
+} else {
+    console.log("Move along")
+}
